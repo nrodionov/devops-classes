e1e468914094fb7dfb759d636405dccfb18e0900
Klim Arbuzov <klim.arbuzov@gmail.com>
2018-01-09 13:10:58 +0300

    First

---
+++ userdata.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+apt-add-repository -y ppa:ansible/ansible
+apt-get update
+apt-get -y install ansible git gosu
+cd /home/ubuntu
+git init
+git remote add origin https://github.com/nrodionov/devops-classes.git
+git fetch
+git checkout -t origin/master
+chown ubuntu:ubuntu /home/ubuntu -R
+gosu ubuntu ansible-playbook -i "localhost," -c local app.yml
